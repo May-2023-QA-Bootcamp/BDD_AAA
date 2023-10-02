beb9d33235c83de0c3de150e864754df7d3e7d3c
Md Nasir Ibne Hussain <md.nasiribnehussain@gmail.com>
2023-10-01 21:46:38 -0400

    Finalizing BDD

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(	tags = "@smoke",
 					features = "src/test/java/features",
-					glue = "stepdefs",
+					glue = {"base","stepdefs"},
 					dryRun = false,
 					monochrome = false,
 					plugin = {"pretty","html:target/report.html","json:target/report.json"}
